87c57e2213b3657826b42ed2cd91f28d01f5e571
i k <drakonello2002@gmail.com>
2021-04-05 14:25:02 +0300

    m

--- server.js
+++ server.js
@@ -37,14 +37,14 @@ async function getTasks(tasksArr,callback){
 			
 			for(let i of tasksArr){
 				
-				tasks.push(allTasks[i]);		
+				tasks[i] = allTasks[i];		
 			}
 			callback(tasks);
 			
 		}
 	});	
 }
-async function modifyTask(task)
+async function modifyTask(task,callback)
 {
 	let progress={
 		"TODO":"DONE",
@@ -53,82 +53,195 @@ async function modifyTask(task)
 	}
 	database.ref('tasks/'+task).once('value')
 	.then(function(snapshot) {
-		console.log(snapshot.val());
+		if(!snapshot.val()){
+			callback(false)
+			return;
+		};
 		firebase.database().ref('tasks/' + task).set({
 			desc:snapshot.val().desc,
 			progress: progress[snapshot.val().progress],
 			person:snapshot.val().person,
 			
 		});
+		callback(true);
 	});
 	
 	
 	
 }
 
-async function addTask(user,task){
+async function addTask(user,task,callback){
 	database.ref('tasks/').once('value')
 	.then(function(snapshot) {
-		let tasks = snapshot.val();
-		tasks.push(task);
+		let tasks = snapshot.val()?snapshot.val():[];
+		let tasksId = tasks.indexOf(null)>0? tasks.indexOf(null):tasks.length;
+		tasks[tasksId] = task;
+		try{
+			//console.log(firebase.database().ref('users/'+task.person+"/tasks/"+user.tasks.length))
+			let index=user.tasks?user.tasks.length:0;
 		
-		firebase.database().ref('tasks/'+tasks.length).set(task);
-		console.log(firebase.database().ref('users/'+task.person+"/tasks/"+user.tasks.length))
-		firebase.database().ref('users/'+task.person+"/tasks/"+user.tasks.length).set(tasks.length);
+			firebase.database().ref('users/'+task.person+"/tasks/"+index).set(tasksId);
+			firebase.database().ref('tasks/'+tasksId).set(task);
+		}catch(e){
+			console.log(e);
+			callback(-1);
+		}
+		callback(tasksId);
 	});
 }
-function deleteTask(user,task){
-	const index = user.tasks.indexOf(task);
-	user.tasks.splice(index, 1);
+async function deleteTask(task){
 	
-	database.ref('tasks/'+task).remove();
-	database.ref('users/'+user.id+"/tasks").set(user.tasks);
+	
+	database.ref('tasks/'+task).once('value').then((snapshot)=>{
+		
+		database.ref('users/'+snapshot.val().person).once('value').then((snapshot2)=>{
+			let user=snapshot2.val();
+			console.log(user);
+			const index = user.tasks.indexOf(task);
+			user.tasks.splice(index,1);
+			
+			database.ref('users/'+snapshot.val().person+"/tasks").set(user.tasks);			
+		});
+		database.ref('tasks/'+task).remove();
+		
+		
+		
+	});
+	
 }
 //=================================================================
 
 let app = express();
 app.use(express.json())   
 app.get('/', async function (req, res) {
-   getUser(req.header('id'),(user)=>{
-	   let end=(t)=>{res.end(JSON.stringify(t));};
-	   if(user.isAdmin){
-		   getTasks(true,end);   
-	   }else{
-		  
-		   getTasks(user.tasks,end);
-	   }
-	      
-   });
+	if(!req.header('id')){
+		res.send(401,"need id in header");
+		return;
+	}
+	try{
+		getUser(req.header('id'),(user)=>{
+			//console.log(user);
+			if(!user){
+				res.send(401,"no such user exists");
+				return;
+			}
+			let end=(t)=>{
+				t.forEach((el,index)=>{
+					if(el)el.id = index;
+				})
+				res.end(JSON.stringify(t.filter((el)=> {
+					return el != null;
+				})));	
+			};
+			if(user.isAdmin){
+			   getTasks(true,end);   
+			}else{
+			   getTasks(user.tasks,end);
+			}
+			  
+		});
+	}catch(e){
+		res.send(400,"bad request(make sure it is json)");
+	}
 });
 app.post('/', function (req, res) {
-   getUser(req.header('id'),(user)=>{
-	   console.log(req.body.task);
-	   if(user.isAdmin||user.tasks.includes(req.body.task)){
-		   modifyTask(req.body.task);	   
-	   }   	   
-   });
-   res.end("OK");
+    if(!req.header('id')){
+	   res.send(401,"need id in header");
+	}
+	try{
+		getUser(req.header('id'),(user)=>{
+			if(!user){
+				res.send(401,"no such user exists");
+				return;
+			}
+			user.tasks=user.tasks?user.tasks:[];
+			if(user.isAdmin||user.tasks.includes(req.body.task)){
+			   modifyTask(req.body.task,(result)=>{
+					if(!result){res.send(404,"task not found");return;}
+					res.end("OK");				 				 		   
+				});
+			   
+				   
+			}else{
+				res.send(401,"unauthorized for operation");
+			}
+	   });
+	}catch(e){
+		res.send(400,"bad request(make sure it is json)");
+	}
+   
 });
 app.put('/', function (req, res) {
-   getUser(req.header('id'),(user)=>{
-	   addTask(user,{
-		    desc:req.body.desc,
-		    person:req.body.person,
-			progress:"TODO"   
-	   })
-   });
-   res.end("OK");
+   if(!req.header('id')){
+	   res.send(401,"need id in header");
+	   return;
+   }
+   try{
+	   getUser(req.header('id'),(user)=>{
+			if(!user){		
+				res.send(401,"no such user exists");
+				return;
+			}
+			//console.log(req.body);
+			if(user.isAdmin||req.header('id')==req.body.person){
+				getUser(req.body.person,(user)=>{
+					if(!user){
+						res.send(400,"no such user exists");
+						return;
+					}
+					addTask(user,{
+						desc:req.body.desc,
+						person:req.body.person,
+						progress:"TODO"   
+					},(task)=>{
+						if(task==-1){
+							res.send(400,"no such user exists");
+							return;
+						}else{
+							res.end("task id:"+task);
+							return;
+						}
+					});
+				});
+			}else{
+				//console.log(req.header('id'),req.body.person);
+			   res.send(401,"unauthorized for operation");
+			   return;
+			}  
+	   });
+   }catch(e){
+		res.send(400,"bad request(make sure it is json)");
+	}
+   
    
 });
 app.delete('/', function (req, res) {
-   getUser(req.header('id'),(user)=>{
-	   user.id=req.header('id');
-	   console.log(req.body.task);
-	   if(user.isAdmin||user.tasks.includes(req.body.task)){
-		   deleteTask(user,req.body.task);	   
-	   }   	   
-   });
-   res.end("OK");
+	if(!req.header('id')){
+		res.send(401,"need id in header");
+		return;
+	}
+	try{
+		getUser(req.header('id'),(user)=>{
+			if(!user){
+				res.send(401,"no such user exists");
+				return;
+			}
+			user.id=req.header('id');
+			console.log(req.body.task);
+			user.tasks=user.tasks?user.tasks:[];
+			if(user.isAdmin||user.tasks.includes(req.body.task)){
+			   deleteTask(req.body.task);	
+			   res.end("OK");  
+			   return;
+			}else{
+			   res.send(401,"unauthorized for operation");
+			   return;
+			}   	   
+		});
+	}catch(e){
+		res.send(400,"bad request(make sure it is json)");
+	}
+    
    
 });
 var server = app.listen(3000);
